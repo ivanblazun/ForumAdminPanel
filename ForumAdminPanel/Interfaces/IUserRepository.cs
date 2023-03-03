54a373c3566f94283f962d4ce7855c27412254d6
Ivan Blažun <ivan.blazun@dignet.hr>
2023-03-03 06:47:24 +0100

    User controller and db context added

---
+++ ForumAdminPanel/Interfaces/IUserRepository.cs
@@ -0,0 +1,16 @@
+﻿using ForumAdminPanel.Models;
+
+namespace ForumAdminPanel.Interfaces
+{
+    public interface IUserRepository
+    {
+        Task<IEnumerable<User>> GetAllUsers();
+        Task<User> GetUserByIdAsync(string id);
+
+        bool AddUser(User user);
+        bool UpdateUser(User user);
+        bool DeleteUser(User user);
+        bool DeleteUserById(int id);
+        bool Save();
+    }
+}
---
+++ ForumAdminPanel/Repository/UserRepository.cs
@@ -0,0 +1,54 @@
+﻿using ForumAdminPanel.Data;
+using ForumAdminPanel.Interfaces;
+using ForumAdminPanel.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ForumAdminPanel.Repository
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public bool AddUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUser(User user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DeleteUserById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<IEnumerable<User>> GetAllUsers()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(string id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public bool Save()
+        {
+            var saved =_context.SaveChanges();
+            return saved> 0 ? true : false;
+        }
+
+        public bool UpdateUser(User user)
+        {
+            _context.Users.Update(user);
+            return Save();
+        }
+    }
+}
